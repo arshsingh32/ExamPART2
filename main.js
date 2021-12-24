9b380e529142d5aea941a19d80399f0ce9c2a602
Arshdeep <arshsingh3223@gmail.com>
2021-12-23 21:04:28 -0800

    coding dojo

---
+++ main.js
@@ -0,0 +1,21 @@
+function penguinTouch () {
+    alert("This game is supported on Linux");
+}
+function addingNumbers(){
+    var number = document.querySelector(".zero");
+    number.innerHTML++;
+}
+const imgArray = ["1.png","2.png","3.png"]
+let imgNum = 0 ;
+
+
+function changeImageRight (){
+    var img = document.querySelector("#slider");
+    img.src = "images/pixel-ninjas-2.png"
+}
+
+function changeImageleft (){
+ 
+        var img = document.querySelector("#slider");
+        img.src = "images/stonepunk.png"
+    }
